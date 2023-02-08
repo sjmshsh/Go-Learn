71869466b32347d8c0ad0524a92a027908bf2a1e
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-08 21:22:49 +0800

    Create golang 协程和channel配合使用.go

---
+++ golang 协程和channel配合使用.go
@@ -0,0 +1,21 @@
+// 写代码实现两个 goroutine，其中一个产生随机数并写入到 go channel 中，另外一个从 channel 中读取数字并打印到标准输出。最终输出五个随机数。
+
+func main() {
+	out := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			out <- rand.Intn(5)
+		}
+		close(out)
+	}()
+	go func() {
+		defer wg.Done()
+		for i := range out {
+			fmt.Println(i)
+		}
+	}()
+	wg.Wait()
+}
