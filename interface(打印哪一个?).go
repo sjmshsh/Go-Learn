b25e9bfb1d2eeb3b43f68c4ba9efcf4c17eb7836
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-08 14:26:11 +0800

    Create interface(打印哪一个?).go

---
+++ interface(打印哪一个?).go
@@ -0,0 +1,24 @@
+type People interface {
+	Show()
+}
+
+type Student struct{}
+
+func (stu *Student) Show() {
+
+}
+
+func live() People {
+	var stu *Student
+	return stu
+}
+
+func main() {
+	if live() == nil {
+		fmt.Println("AAAAAAA")
+	} else {
+		fmt.Println("BBBBBBB")
+	}
+}
+
+// 答案是打印BBBBBB
