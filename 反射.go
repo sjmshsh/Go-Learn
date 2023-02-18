434f7890bbe6483b70ed6db7e5e323f7b0c44e06
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-18 14:07:08 +0800

    Create 反射.go

---
+++ 反射.go
@@ -0,0 +1,25 @@
+func main() {
+	var wg sync.WaitGroup
+	typ := reflect.TypeOf(&wg)
+	for i := 0; i < typ.NumMethod(); i++ {
+		method := typ.Method(i)
+		argv := make([]string, 0, method.Type.NumIn())
+		returns := make([]string, 0, method.Type.NumOut())
+		// j 从 1 开始，第 0 个入参是 wg 自己。
+		for j := 1; j < method.Type.NumIn(); j++ {
+			argv = append(argv, method.Type.In(j).Name())
+		}
+		for j := 0; j < method.Type.NumOut(); j++ {
+			returns = append(returns, method.Type.Out(j).Name())
+		}
+		log.Printf("func (w *%s) %s(%s) %s",
+			typ.Elem().Name(),
+			method.Name,
+			strings.Join(argv, ","),
+			strings.Join(returns, ","))
+	}
+}
+
+// 2023/02/18 14:05:36 func (w *WaitGroup) Add(int)
+// 2023/02/18 14:05:36 func (w *WaitGroup) Done() 
+// 2023/02/18 14:05:36 func (w *WaitGroup) Wait() 
