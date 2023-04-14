96478ddbb3854ff01ee220a53642e412fbea9ffd
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-04-14 15:53:22 +0800

    Create web.go

---
+++ web.go
@@ -0,0 +1,11 @@
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	server := &http.Server{
+		Addr: "0.0.0.0:80",
+	}
+	http.HandleFunc("/", hello)
+	server.ListenAndServe()
+}
