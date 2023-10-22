f17dda676a3033ee320620042c154e19cb4fe811
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-10-23 01:20:20 +0800

    Create  启动 2个groutine 2秒后取消， 第一个协程1秒执行 完，第二个协程3秒执行完.go

---
+++ 启动 2个groutine 2秒后取消， 第一个协程1秒执行 完，第二个协程3秒执行完.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func f1(in chan struct{}) {
+	time.Sleep(1 * time.Second)
+	in <- struct{}{}
+}
+
+func f2(in chan struct{}) {
+	time.Sleep(3 * time.Second)
+	in <- struct{}{}
+}
+
+func main() {
+	ch1 := make(chan struct{})
+	ch2 := make(chan struct{})
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+
+	go func() {
+		go f1(ch1)
+		select {
+		case <-ctx.Done():
+			fmt.Println("f1 timeout")
+			break
+		case <-ch1:
+			fmt.Println("f1 done")
+		}
+	}()
+
+	go func() {
+		go f2(ch2)
+		select {
+		case <-ctx.Done():
+			fmt.Println("f2 timeout")
+			break
+		case <-ch2:
+			fmt.Println("f2 done")
+		}
+	}()
+
+	time.Sleep(time.Second * 5)
+}
